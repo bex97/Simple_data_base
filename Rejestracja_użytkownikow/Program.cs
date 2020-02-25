9ba7e34352a52414d970b29cea7d03ab32fe4a00
Benio <ben97.pigan@gmail.com>
2020-02-25 17:55:06 +0100

    Pierwsze kroki...

---
+++ Rejestracja_użytkownikow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rejestracja_użytkownikow
+{
+
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string conn_string = @" Data Source = (LocalDB)\MSSQLLocalDB; " +
+                                 @" AttachDbFilename = D:\Projekty\C#\Rejestracja_użytkownikow\Rejestracja_użytkownikow\Database1.mdf;" +
+                                 @" Integrated Security = True";
+            ConnectionSQL.connect_to_database(conn_string);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Rejestracja_użytkownikow/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Imie] NVARCHAR(50) NOT NULL, 
+    [Nazwisko] NVARCHAR(50) NOT NULL, 
+    [E-mail] NVARCHAR(50) NOT NULL, 
+    [Plec] CHAR(10) NULL, 
+    [Adres] NVARCHAR(50) NOT NULL, 
+    [Telefon] NVARCHAR(50) NOT NULL
+)
